d6d573375c5bb0fa43ee8c66dfcd3de015312318
Jiovani Rivera <106453116+JioR95@users.noreply.github.com>
2025-07-18 23:28:40 -0400

    first commit

---
+++ src/MyComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default "MyComponent";
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Hello this my First React Component</h1>
+    </div>
+  );
+};
